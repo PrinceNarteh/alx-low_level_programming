186c342e8a81ac518a449e4af01049dd33885df9
Prince Narteh <prinarteh@gmail.com>
2023-01-09 09:01:45 -0800

    update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,13 @@
   * main - Print the number of arguments passed to the programm
   * @argc: Number of arguements
   * @argv: Array of pointers of Arguements
-  * 
+  *
   * Return: Always 0 (success)
   */
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	print("%d\n", argc);
+	(void) argv;
+	print("%d\n", argc - 1);
 
 	exit(EXIT_SUCCESS);
 }
