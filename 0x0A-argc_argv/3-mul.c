a17565afcc2b0187162c60fb3c6be8235049d414
Prince Narteh <prinarteh@gmail.com>
2023-01-09 09:42:59 -0800

    update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
   * main - Print the the multiplication of two numbers
   * @argc: Number of arguments
   * @argv: Array of pointers of Arguments
   *
-  * Return: Always 0 (success)
+  * Return: 0 if success, 1 if failure
   */
 int main(int argc, char *argv[])
 {
@@ -15,12 +14,12 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("Error\n");
-		exit(EXIT_FAILURE);
+		return (1);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 
 	printf("%i\n", a * b);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
