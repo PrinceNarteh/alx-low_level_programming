5b8f6c4e05f8989922d5da3977daa13023112c78
Prince Narteh <prinarteh@gmail.com>
2023-01-09 09:17:43 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * main - Print the number of arguments passed to the programme
+  * @argc: Number of arguments
+  * @argv: Array of pointers of Arguments
+  *
+  * Return: Always 0 (success)
+  */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		print("%s\n", *argv++);
+
+	exit(EXIT_SUCCESS);
+}
