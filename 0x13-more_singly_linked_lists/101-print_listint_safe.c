671e06d70347e465e08360da0018e15f37bd16e7
Prince Narteh <prinarteh@gmail.com>
2023-01-31 19:43:35 -0800

    update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * _realloc - reallocates memory to the nodes in a linked list
+ * @l: the list to append
+ * @s: size of the new list
+ * @n: new node to add
  *
- * Return: number of nodes in the list
+ * Return: pointer to the new list
+ */
+const listint_t **_realloc(const listint_t **l, size_t s, const listint_t *n)
+{
+	const listint_t **list;
+	size_t i;
+
+	list = malloc(s * sizeof(listint_t *));
+	if (list == NULL)
+	{
+		free(l);
+		exit(98);
+	}
+	for (i = 0; i < s - 1; i++)
+		list[i] = l[i];
+	list[i] = n;
+	free(l);
+	return (list);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the linked list's struct
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t num = 0;
-    long int diff;
+	size_t a, len = 0;
+	const listint_t **l = NULL;
 
-    while (head)
-    {
-        diff = head - head->next;
-        num++;
-        printf("[%p] %d\n", (void *)head, head->n);
-        if (diff > 0)
-            head = head->next;
-        else
-        {
-            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-            break;
-        }
-    }
-
-    return (num);
+	while (head != NULL)
+	{
+		for (a = 0; a < len; a++)
+		{
+			if (head == l[a])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(l);
+				return (len);
+			}
+		}
+		len++;
+		l = _realloc(l, len, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	free(l);
+	return (len);
 }
